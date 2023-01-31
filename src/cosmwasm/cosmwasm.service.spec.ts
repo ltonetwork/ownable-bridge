5b4d4706e221cbf893275e6b377537b919666e10
arnold <arnold@jasny.net>
2023-01-30 20:13:35 -0400

    Added tests for CosmWasm service.

--- src/cosmwasm/cosmwasm.service.spec.ts
+++ src/cosmwasm/cosmwasm.service.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CosmWasmService } from './cosmwasm.service';
+import * as assert from 'assert';
+import Contract from './contract';
 
-describe('CosmwasmService', () => {
+describe('CosmWasmService', () => {
   let service: CosmWasmService;
+  let contract: Contract;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,7 +15,43 @@ describe('CosmwasmService', () => {
     service = module.get<CosmWasmService>(CosmWasmService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  beforeEach(async () => {
+    contract = await service.load(
+      __dirname + '/_test/ownable.js',
+      __dirname + '/_test/ownable_bg.wasm',
+    );
+  });
+
+  it('instantiates the smart contract', async () => {
+    const result = await contract.instantiate(
+      { ownable_id: '123', network_id: 'T' },
+      { sender: 'some_account', funds: [] },
+    );
+    assert.equal(result.method, 'instantiate');
+    assert.equal(result.owner, 'some_account');
+    assert.equal(result.current_amount, '100');
+
+    const state = await contract.query({ get_ownable_config: {} });
+    assert.equal(state.owner, 'some_account');
+    assert.equal(state.current_amount, '100');
+    assert.equal(state.color, result.color);
+  });
+
+  it('executes a message on the smart contract', async () => {
+    await contract.instantiate(
+      { ownable_id: '123', network_id: 'T' },
+      { sender: 'some_account', funds: [] },
+    );
+
+    const result = await contract.execute(
+      { consume: { amount: 42 } },
+      { sender: 'some_account', funds: [] },
+    );
+    assert.equal(result.method, 'try_consume');
+    assert.equal(result.new_amount, '58');
+
+    const state = await contract.query({ get_ownable_config: {} });
+    assert.equal(state.owner, 'some_account');
+    assert.equal(state.current_amount, '58');
   });
 });
