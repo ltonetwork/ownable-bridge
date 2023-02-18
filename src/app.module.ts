894db2fad760d9d11f0c85b41dfa7560af867c95
arnold <arnold@jasny.net>
2023-02-17 22:21:59 -0400

    No need to load EthersModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { AppService } from './app.service';
 import { PackageModule } from './package/package.module';
 import { OwnableModule } from './ownable/ownable.module';
 import { ConfigModule } from './common/config/config.module';
-import { EthersModule } from './common/ethers/ethers.module';
 
 @Module({
-  imports: [ConfigModule, PackageModule, OwnableModule, EthersModule],
+  imports: [ConfigModule, PackageModule, OwnableModule],
   controllers: [AppController],
   providers: [AppService],
 })
