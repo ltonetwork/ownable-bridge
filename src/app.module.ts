2f248abc4bcd77c7e966da6676c66e7c6b428cf8
arnold <arnold@jasny.net>
2024-01-31 14:19:11 +0100

    Missing dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './common/config/config.module';
+import { LtoModule } from './common/lto/lto.module';
 import { PackageModule } from './package/package.module';
 import { OwnableModule } from './ownable/ownable.module';
 import { OwnableController } from './ownable/ownable.controller';
 import { VerifySignatureMiddleware } from './common/http-signature/verify-signature.middleware';
 
 @Module({
-  imports: [ConfigModule, PackageModule, OwnableModule],
+  imports: [ConfigModule, PackageModule, OwnableModule, LtoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/ownable/ownable.module.ts
+++ src/ownable/ownable.module.ts
@@ -5,10 +5,13 @@ import { CosmWasmModule } from '../cosmwasm/cosmwasm.module';
 import { PackageModule } from '../package/package.module';
 import { ConfigModule } from '../common/config/config.module';
 import { EthersModule } from '../common/ethers/ethers.module';
+import { NFTModule } from '../nft/nft.module';
+import { LtoIndexModule } from '../common/lto-index/lto-index.module';
 import { OwnableService } from './ownable.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule, LtoModule, CosmWasmModule, PackageModule, EthersModule],
+  imports: [ConfigModule, LtoModule, CosmWasmModule, PackageModule, EthersModule, NFTModule, LtoIndexModule, HttpModule],
   providers: [OwnableService],
   controllers: [OwnableController],
 })
